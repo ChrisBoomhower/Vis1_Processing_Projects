086ab0a9ac42377c5e85d3b8cd439c804b12d4b6
Amfrye777 <Amfrye@smu.edu>
2017-07-09 14:03:46 -0500

    3 / 4 data aggregation files created ExecPython class created

--- MidtermProject_Boomhower_Frye/MidtermProject_Boomhower_Frye.pde
+++ MidtermProject_Boomhower_Frye/MidtermProject_Boomhower_Frye.pde
@@ -1,26 +1,20 @@
-/** 
- * Python Command (v1.0.1)
- * GoToLoop (2017/Jul/05)
- *
- * https://Forum.Processing.org/two/discussion/23324/
- * executing-python-3-code-from-within-processing#Item_9
- *
- * https://GitHub.com/GoToLoop/command/blob/patch-1/src/
- * deadpixel/command/Command.java
- */
- 
-import deadpixel.command.Command;
-Command cmd;
- 
-String APP  = "python ";
-String FILE = "parseData.py";
+/****************************************************************************************** //<>// //<>// //<>//
+ Title         : OPM Separation Analysis Dashboard
+ Created By    : Chris Boomhower, Alex Frye
+ Create Date   : 7/9/2017
+ Assignment    : MSDS6390 - Midterm Project
+ Resources     : https://Forum.Processing.org/two/discussion/23324/executing-python-3-code-from-within-processing#Item_9
+                 https://GitHub.com/GoToLoop/command/blob/patch-1/src/deadpixel/command/Command.java
+ ******************************************************************************************/
+
+ExecPython execPython;
  
 void setup() {
-  String dataPath = "cd /D \""+dataPath("")+ "\"";
-  String PythonFilesPath = sketchPath()+"\\PythonFiles\\";
 
-  cmd = new Command("cmd.exe /c " + dataPath + " && " + APP + "\"" + PythonFilesPath + FILE + "\"" + " 1" + " 0");
-  
-  boolean success = cmd.run();
-  print("Success:", success);
+  //these variables will be replaced by inputs from class object variable getter method calls. 
+  //no need to assign to variable... just exec the declaration method each time we need to run new files
+  //setup will need to establish default values for all these class variables for the FULL Dataset. 
+    //Then actions will update the variables and resubmit thefile generation
+new ExecPython(startQTR, endQTR, SEP_NS, SEP_SA, SEP_SC, SEP_SD, SEP_SH, SEP_SI, SEP_SJ);
+
 }
--- MidtermProject_Boomhower_Frye/PythonFiles/parseData.py
+++ MidtermProject_Boomhower_Frye/PythonFiles/parseData.py
@@ -1,12 +1,90 @@
 #!/usr/bin/env python
 
+## Execution Syntax
+#### python parseData.py 1 4 NS SA SC SD SH SI SJ
+#### python parseData.py startQTR endQTR SEP1 SEP2 SEP3 ...
+
+import os
+import glob
 import sys
 import pandas as pd
+import numpy as np
 
-print(sys.argv[1], sys.argv[2])
+startQTR = int(sys.argv[1])
+endQTR = int(sys.argv[2])
 
-df = pd.DataFrame([sys.argv[1], sys.argv[2]])
+SEPList = []
 
-print(df)
+for i in range(3,len(sys.argv)):
+    SEPList.append(sys.argv[i])
 
-df.to_csv('test.csv', index = False)
+print(SEPList)
+
+
+OPMDataFiles = glob.glob(os.path.join("../data", "SampledOPMData*.csv"))
+
+for i in range(0,len(OPMDataFiles)):
+    OPMDataFiles[i] = OPMDataFiles[i].replace("\\","/")
+
+print(OPMDataFiles)
+
+SampledOPMDataList = []
+
+for i,j in zip(OPMDataFiles,range(0,len(OPMDataFiles))):
+    SampledOPMDataList.append(pd.read_csv(i, dtype = 'str'))
+
+SampledOPMData = pd.concat(SampledOPMDataList).reset_index()
+
+
+#= pd.read_csv("../data/SampledOPMData.csv", dtype = 'str')
+
+SampledOPMData[['SALARY', 'LOS', 'SEPCount_EFDATE_OCC', 'SEPCount_EFDATE_LOC', 'IndAvgSalary', 'SalaryOverUnderIndAvg',
+                'BLS_FEDERAL_OtherSep_Rate', 'BLS_FEDERAL_Quits_Rate', 'BLS_FEDERAL_TotalSep_Level', 'BLS_FEDERAL_JobOpenings_Rate',
+                'BLS_FEDERAL_OtherSep_Level', 'BLS_FEDERAL_Quits_Level', 'BLS_FEDERAL_JobOpenings_Level', 'BLS_FEDERAL_Layoffs_Rate',
+                'BLS_FEDERAL_Layoffs_Level', 'BLS_FEDERAL_TotalSep_Rate', 'QTR']] \
+    = SampledOPMData[['SALARY', 'LOS', 'SEPCount_EFDATE_OCC', 'SEPCount_EFDATE_LOC', 'IndAvgSalary', 'SalaryOverUnderIndAvg',
+                'BLS_FEDERAL_OtherSep_Rate', 'BLS_FEDERAL_Quits_Rate', 'BLS_FEDERAL_TotalSep_Level', 'BLS_FEDERAL_JobOpenings_Rate',
+                'BLS_FEDERAL_OtherSep_Level', 'BLS_FEDERAL_Quits_Level', 'BLS_FEDERAL_JobOpenings_Level', 'BLS_FEDERAL_Layoffs_Rate',
+                'BLS_FEDERAL_Layoffs_Level', 'BLS_FEDERAL_TotalSep_Rate', 'QTR']].apply(pd.to_numeric)
+
+
+OPMDataFiltered = SampledOPMData[(SampledOPMData["QTR"] >= startQTR) & (SampledOPMData["QTR"] <= endQTR)
+                                 & (SampledOPMData["SEP"].isin(SEPList))]
+
+print(len(OPMDataFiltered))
+
+## Average Salary Over Under by Occupation Family
+
+AvgSalOverUnderByOCCFAMT = pd.DataFrame({'CNT' : OPMDataFiltered.groupby(["OCCFAMT"]).size(),
+                                         'SUM' : OPMDataFiltered.groupby(["OCCFAMT"])["SalaryOverUnderIndAvg"].sum()}).reset_index()
+AvgSalOverUnderByOCCFAMT["AvgSalOverUnder"] = AvgSalOverUnderByOCCFAMT["SUM"]/AvgSalOverUnderByOCCFAMT["CNT"]
+AvgSalOverUnderByOCCFAMT["Magnitude"]=np.where(AvgSalOverUnderByOCCFAMT["AvgSalOverUnder"]<0,-AvgSalOverUnderByOCCFAMT["AvgSalOverUnder"],AvgSalOverUnderByOCCFAMT["AvgSalOverUnder"])
+AvgSalOverUnderByOCCFAMT = AvgSalOverUnderByOCCFAMT.sort_values("Magnitude", ascending=False).reset_index(drop=True)
+del AvgSalOverUnderByOCCFAMT["CNT"], AvgSalOverUnderByOCCFAMT["SUM"], AvgSalOverUnderByOCCFAMT["Magnitude"]
+
+AvgSalOverUnderByOCCFAMT.to_csv('../data/AvgSalOverUnderByOCCFAMT.csv', index = False)
+
+
+## BLS Data Bubbles
+
+AvgBLS = pd.DataFrame({'CNT' : len(OPMDataFiltered),
+                       'SUM' : OPMDataFiltered[['BLS_FEDERAL_TotalSep_Level', 'BLS_FEDERAL_JobOpenings_Level', 'BLS_FEDERAL_Layoffs_Level', 'BLS_FEDERAL_Quits_Level', 'BLS_FEDERAL_OtherSep_Level',
+                                                'BLS_FEDERAL_TotalSep_Rate',  'BLS_FEDERAL_JobOpenings_Rate',  'BLS_FEDERAL_Layoffs_Rate',  'BLS_FEDERAL_Quits_Rate',   'BLS_FEDERAL_OtherSep_Rate']].sum()})
+
+AvgBLS["Avg"] = AvgBLS["SUM"]/AvgBLS["CNT"]
+del  AvgBLS["SUM"], AvgBLS["CNT"]
+
+AvgBLS = AvgBLS.transpose()
+AvgBLS.to_csv('../data/AvgBLS.csv', index = False)
+
+
+## Guardian Distinct Combinations
+
+DistinctOCCFAMT_AGELVL_LOC_SALLVL = pd.DataFrame({'CNT' : OPMDataFiltered.groupby(["OCCFAMT", "AGELVLT", "LOCT", "SALLVLT"]).size()}).reset_index()
+DistinctOCCFAMT_AGELVL_LOC_SALLVL = DistinctOCCFAMT_AGELVL_LOC_SALLVL.sort_values("CNT", ascending=False).reset_index(drop=True)
+DistinctOCCFAMT_AGELVL_LOC_SALLVL.to_csv('../data/DistinctOCCFAMT_AGELVL_LOC_SALLVL.csv', index = False)
+
+
+
+
+
